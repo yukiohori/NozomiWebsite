1b2adf2d5c65e8a968f1170edae38af10d85ac6a
YOS <yuki8963@gmail.com>
2017-09-20 03:04:10 -0700

    home top finished

--- src/home.jsx
+++ src/home.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import '../styles/index.scss';
 import {Carousel , Row, Col} from 'react-materialize';
 
-const maxX = 10;
-const maxY = 10;
+const transformFactor = 0.11;
+const transformFactor2 = 0.2;
+const shadowFactor = 2;
+const scale = (n, min, max) => n * (max - min) + min;
+const calculateAngle = (x, y, width, height) => Math.atan2(x - (width / 2), -(y - (height / 2))) * (180 / Math.PI);
 
 export default class App extends React.Component {
 	constructor(props) {
@@ -12,15 +15,17 @@ export default class App extends React.Component {
 		this.state = {
 			worksSelected: {
 				works: 'selected',
-				gallery:'',
-				pageX: 0,
-				pageY: 0
+				gallery:''
 			},
 			worksShow:'works',
 			animationArray:{
 				homecontent:'',
 				blogcontent:'',
-			}
+			},
+			topImage: 'matrix3d(1, 0, 0, 0,0, 1, 0, 0,0, 0, 1, 0,0, 0, 0, 1)',
+			topLetter: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)',
+			topPosition: '',
+			boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)',
 		}
 
 		this.onMouseMove = this.onMouseMove.bind(this);
@@ -52,20 +57,36 @@ export default class App extends React.Component {
 	}
 
 	onMouseMove(e){
+
+		const x = e.pageX;
+		const y = e.pageY;
+
+		const scaledX = scale(x / this.refs.imagehover.clientWidth, -1, 1);
+		const scaledY = scale(y / this.refs.imagehover.clientHeight, -1, 1);
+
+		const maxX = Math.atan2(15, this.refs.imagehover.clientHeight * .5 * 180 / Math.PI);
+		const maxY = Math.atan2(15, this.refs.imagehover.clientWidth * .5 * 180 / Math.PI);
+		const opacity = Math.max(Math.abs(scaledX / maxX), Math.abs(scaledY / maxY));
+
+		const angle = calculateAngle(x, y, this.refs.imagehover.clientWidth, this.refs.imagehover.clientHeight);
+
 		this.setState({
-			pageX: e.pageX,
-			pageY: e.pageY
+			topImage: 'matrix3d(1, 0, '+(-scaledX * transformFactor)+', 0,0, 1, '+(-scaledY * transformFactor)+', 0,0, 0, 1, 0,0, 0, 0, 1)',
+			topLetter: 'matrix3d(1, 0, '+(-scaledX * transformFactor2)+', 0,0, 1, '+(-scaledY * transformFactor2)+', 0, 0, 0, 1, 0,0, 0, 0, 1)',
+			topPosition: 'translateX(0px) translateY('+(-scaledY * 20)+'px) translateZ(150px)',
+			boxShadow: (-scaledX * shadowFactor)+'px '+(-scaledY * shadowFactor)+'px '+(30)+'px rgba(0, 0, 0,'+(opacity * 2)
 		});
-		// console.log(e);
 	}
 
 	componentDidMount() {
-		window.addEventListener("mousemove", this.onMouseMove);
+		this.refs.imagehover.addEventListener("mousemove", this.onMouseMove);
+		// window.addEventListener("mousemove", this.onMouseMove);
     // window.addEventListener("scroll", this.handleScroll);
   }
 
   componentWillUnmount() {
-		window.removeEventListener("mousemove", this.onMouseMove);
+		this.refs.imagehover.removeEventListener("mousemove", this.onMouseMove);
+		// window.removeEventListener("mousemove", this.onMouseMove);
     // window.removeEventListener("scroll", this.handleScroll);
   }
 
@@ -88,24 +109,56 @@ export default class App extends React.Component {
       <div className="backgournd-content">
 				<div className="container-style">
 					<Row>
-						<div className="top-header">
-							<h1>NOZOMI NAKANO</h1>
-							<img style={{display: 'block'}} src={require('../img/demo.png')} alt="" />
+						<div ref="imagehover" className="top-header">
+							<div className="top-letter" style={{transform: this.state.topPosition}}>
+								<h1 style={{transform: this.state.topLetter}}>NOZOMI NAKANO</h1>
+							</div>
+							<img style={{transform: this.state.topImage, boxShadow: this.state.boxShadow}} src={require('../img/demo.png')} alt="" />
 						</div>
 					</Row>
 					<Row>
-						<h1>NOZOMI NAKANO</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Aenean mauris nisi, maximus a placerat at, scelerisque sed orci.
-								Pellentesque mi nunc, auctor ut sollicitudin sed, iaculis tristique dolor.
-						</p>
+						<Col s={12} m={2} l={3}></Col>
+						<Col s={12} m={8} l={6}>
+							<hr />
+						</Col>
+						<Col s={12} m={2} l={3}></Col>
+					</Row>
+					<Row className="about-home">
+						<Col s={12} m={2} l={3}></Col>
+						<Col s={12} m={8} l={6}>
+							<h1>NOZOMI NAKANO</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Aenean mauris nisi, maximus a placerat at, scelerisque sed orci.
+									Pellentesque mi nunc, auctor ut sollicitudin sed, iaculis tristique dolor.
+							</p>
+						</Col>
+						<Col s={12} m={2} l={3}></Col>
 					</Row>
 					<Row>
-						<h1>NOZOMI NAKANO</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Aenean mauris nisi, maximus a placerat at, scelerisque sed orci.
-								Pellentesque mi nunc, auctor ut sollicitudin sed, iaculis tristique dolor.
-						</p>
+						<Col s={12} m={2} l={3}></Col>
+						<Col s={12} m={8} l={6}>
+							<hr />
+						</Col>
+						<Col s={12} m={2} l={3}></Col>
+					</Row>
+					<Row className="about-home">
+						<Col s={12} m={12} l={6}>
+							<img src={require('../img/demo.png')} alt="" />
+						</Col>
+						<Col s={12} m={12} l={6} className="content-centered">
+							<h1>NOZOMI NAKANO</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Aenean mauris nisi, maximus a placerat at, scelerisque sed orci.
+									Pellentesque mi nunc, auctor ut sollicitudin sed, iaculis tristique dolor.
+							</p>
+						</Col>
+					</Row>
+					<Row>
+						<Col s={12} m={2} l={3}></Col>
+						<Col s={12} m={8} l={6}>
+							<hr />
+						</Col>
+						<Col s={12} m={2} l={3}></Col>
 					</Row>
 				</div>
       </div>
